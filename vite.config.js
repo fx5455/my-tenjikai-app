ac618aeeb406e522853f6dfda460b88fe5ee6beb
fx545 <fx545@yahoo.co.jp.>
2025-05-01 20:52:11 +0900

    fix: correct vite.config.js and include react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // react-router-dom を必ずプリバンドル
   optimizeDeps: {
-    include: ['react-router-dom'],
-  },
-})
+    include: ['react-router-dom']
+  }
+});
